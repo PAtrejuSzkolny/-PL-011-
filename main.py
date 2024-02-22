c82ce5200e6ce0b7f61e68f890b0ac708d0eb370
PAtrejuSzkolny <148434820+PAtrejuSzkolny@users.noreply.github.com>
2024-02-22 09:34:11 +0100

    Zadanie 1

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-def get_liczby():
-    with open('.\liczby.txt', 'r') as file:
-        liczby = file.read().strip().split(';')
-        final = []
-        for i, val in enumerate(liczby):
-            try:
-                final.append(int(val))
-            except Exception:
-                raise Exception(f"Invalid input, can't make int out of '{val}'")
-        try:
-            if
 
+def sum_file(file):
+    sum = 0
+    try:
+        with open(file, 'r') as file:
+            data = file.read().strip().split(';')
+            if len(data) == 0 and data[0] == '':
+                return 0
+            liczby = []
+            for i, val in enumerate(data):
+                if data[i].isalpha():
+                    raise TypeError('Plik zawiera litery, a powinny być same liczby')
 
-
-
-
-print(get_liczby())
-
-
+                if int(data[i]) < -10 or int(data[i]) > 10:
+                    raise ValueError('Liczba musi być w przedziale od [-10; 10]')
+                liczby.append(int(data[i]))
+            for j in liczby:
+                sum += j
+    except FileNotFoundError:
+        raise FileNotFoundError('Nie ma takiego pliku')
+    return sum
--- tests/test_Zadanie1.py
+++ tests/test_Zadanie1.py
@@ -1,2 +1,47 @@
 from unittest import TestCase
-from main import
+from main import sum_file
+
+class Test(TestCase):
+    def test_sum(self):
+        # Given
+        file = "test_sum_input.txt"
+        expected = 3
+        # When
+        result = sum_file(file)
+        # Then
+        self.assertEqual(expected, result)
+
+    def test_sum_wrong_range(self):
+        # Given
+        file = "test_sum_wrong_range_input.txt"
+        # When
+        with self.assertRaises(ValueError) as cm:
+        # Then
+            sum_file(file)
+            self.assertEqual("Liczba musi być w przedziale od [-10; 10]", cm.exception)
+
+    def test_sum_text_given(self):
+        # Given
+        file = "test_sum_text_given_input.txt"
+        # When
+        with self.assertRaises(TypeError):
+            # Then
+            sum_file(file)
+
+    def test_sum_pusty_plik(self):
+        # Given
+        file = "test_sum_pusty_plik.txt"
+        expected = 0
+        # When
+        result = sum_file(file)
+        # Then
+        self.assertEqual(expected, result)
+
+    def test_sum_plik_nie_istnieje(self):
+        # Given
+        file = "test_sum_plik_nie_istnieje_input.txt"
+        expected = 0
+        # When
+        with self.assertRaises(FileNotFoundError):
+            # Then
+            sum_file(file)
